ed41e3cb28f30c2ead108e9d1af2dc8e89768c82
nusoff01 <nicholas.usoff@tufts.edu>
2014-07-17 13:28:58 -0400

    changed file set ups, started working on adding passport

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+var port  = 5000;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash 	 = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+
+//config
+
+mongoose.connect(process.env.MONGOLAB_URI ||
+                 process.env.MONGOHQ_URL  ||
+                 'mongodb://localhost/database');
+
+//require('./config/passport')(passport);
+
+//express application setup
+app.use(morgan('dev')); //log requests to the console
+app.use(cookieParser()); //read cookies
+app.use(bodyParser()); //info from html forms
+
+app.set('view engine', 'ejs'); //ejs for templating
+
+app.use(session({ secret: 'whodafuqmitthinktheyare' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes 
+require('./routes/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch 
+app.listen(port);
+console.log('Using port ' + port);
