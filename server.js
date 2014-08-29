3db914e53fb7387f3154092dfedef3cf6f7de68c
Nicholas Usoff <Nick@Tylers-MacBook-Pro.local>
2014-08-29 16:40:56 -0400

    again changing env variables

--- server.js
+++ server.js
@@ -16,8 +16,7 @@ var listing      = require('./lib/listing.js');
 
 //config
 
-mongoose.connect(process.env.MONGOLAB_URI ||
-                 process.env.MONGOHQ_URL  ||
+mongoose.connect( process.env.MONGOHQ_URL  ||
                  'mongodb://localhost/tuftsText');
 // mongoose.connect(process.env.MONGOLAB_URI);
 require('./config/passport')(passport);
